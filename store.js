5535a681d7012040083c83595bb0d7e7423e975c
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-06-15 01:41:48 -0700

    create authenticate_user method for sign-in

--- store.js
+++ store.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 
 module.exports = {
     saltHashPassword,
+    authenticate_user,
     createUser({ first_name, last_name, email, password, address }) {
         const { secret_key = salt, hash } = saltHashPassword(password);
         console.log(`create user, ${first_name} ${last_name}, with email ${email} and password ${password}`)
@@ -21,8 +22,7 @@ const randomString = () => {
     return crypto.randomBytes(4).toString('hex');
 }
 
-function saltHashPassword(password) {
-    const secret_key = randomString();
+function saltHashPassword(password, secret_key = randomString()) {
     const hash = crypto.createHmac('sha256', secret_key).update(password).digest('hex');
 
     return {
@@ -30,3 +30,14 @@ function saltHashPassword(password) {
     }
 }
 
+function authenticate_user({ email, password }) {
+    console.log(`Authenticating user ${ email } `);
+    return knex('users').where({ email })
+        .then( ([user]) => {
+            if(!user) return { success: false }
+            
+            const { hash } = saltHashPassword({ password, secret_key: user.salt })
+            
+            return { success: hash === user.encrypted_password }
+        })
+}
