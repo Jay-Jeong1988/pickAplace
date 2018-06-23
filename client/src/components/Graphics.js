6485a7050a9792987732d4fb81d598b07ba64e64
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-06-23 08:30:11 -0700

    apply d3 to draw a bar chart(incomplete)(second)

--- client/src/components/Graphics.js
+++ client/src/components/Graphics.js
@@ -3,8 +3,8 @@ import * as d3 from 'd3';
 import { Restaurant } from '../lib/requests';
 
 const margin = { top: 20, right: 20, bottom: 70, left: 40};
-const width = 900 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const width = 700 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
 
 class Graphics extends Component {
@@ -17,10 +17,10 @@ class Graphics extends Component {
         }
         this.x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
         this.y = d3.scaleLinear().range([height, 0]);
-        this.xAxis = d3.axisBottom(this.x).ticks(10);
+        this.xAxis = d3.axisBottom(this.x);
         this.yAxis = d3.axisLeft(this.y).ticks(10);
         this.renderChart = this.renderChart.bind(this);
-        
+           
     }
     
     componentDidMount() {
@@ -29,7 +29,6 @@ class Graphics extends Component {
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr( "transform", "translate(" + margin.left + "," + margin.top + ")");
-
         this.setState({
             dummyData: [
                 {
@@ -61,23 +60,74 @@ class Graphics extends Component {
     
     componentDidUpdate(){
         this.renderChart();
+        
     }
     
     renderChart() {
         
-        this.svg.selectAll('circle').data(this.state.dummyData)
-        .enter()
-        .append('circle').attr('transform', d => `translate(${d.price}px, 20px)`)
-        .attr('cx',32).attr('cy',53).attr('r',15);
+        // this.svg.selectAll('g')
+        // .data(this.state.dummyData)
+        // .enter()
+        // .append('circle').attr('transform', d => `translate(${d.price}, 20)`)
+        // .attr('cx',32).attr('cy',53).attr('r',15);
+        this.state.dummyData.forEach( (data) => {
 
-        this.svg.selectAll('g')
-        .data(this.state.dummyData)
-        .enter()
-        .append('g')
-        .attr('class', 'x axis')
-        .call(this.xAxis)
-        
-        //     this.svg.selectAll('g').data(this.state.dummyData, (data) => {
+            this.x.domain([0, 2]);
+            this.y.domain([0, 100]);
+
+            this.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(this.xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", "-.55em")
+            .attr("transform", "rotate(-90)" );
+
+            this.svg.append("g")
+            .attr("class", "y axis")
+            .call(this.yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Value ($)");
+
+            this.svg.selectAll("bar")
+            .data(data)
+            .enter().append("rect")
+            .style("fill", "steelblue")
+            .attr("x", function(d) { return this.x(d.name); })
+            .attr("width", this.x.bandwidth())
+            .attr("y", function(d) { return this.y(d.price); })
+            .attr("height", function(d) { return height - this.y(d.price); });
+        })
+        // this.x.domain( this.state.dummyData.map(d => d.name ));
+        // this.y.domain([0, 100]);
+
+        // this.svg.selectAll('g')
+        //     .data(this.state.dummyData)
+        //     .enter()
+        // .append('g')
+        //     .attr('class', 'x axis')
+        //     .call(this.xAxis)
+        // .selectAll("text")
+        //     .style("text-anchor", "end")
+        //     .attr("dx", "-.8em")
+        //     .attr("dy", "2em");
+
+        // this.svg.selectAll('g')
+        //     .attr('class', 'y axis')
+        //     .call(this.yAxis)
+        // .append('text')
+        //     .attr('transform', 'rotate(-90)')
+        //     .attr('y', 6)
+        //     .attr('dy', '.71em')
+        //     .style('text-anchor', 'end')
+        //     .text('Value (%)');
+
     
         //     this.x.domain(data.map(d => d.name));
         //     this.y.domain([0, d3.max(data, d => d.price)]);
@@ -117,8 +167,10 @@ class Graphics extends Component {
 
         return (
             <main>
+
                 <svg ref="container">
                 </svg>
+
             </main>
         )
     }
