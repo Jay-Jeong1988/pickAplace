1011ce7dccc1df63256cc5d8124f455f6afaf9ca
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-06-24 06:30:59 -0700

    understand how to use svg with d3/ create x and y axis and display bars

--- client/src/components/Graphics.js
+++ client/src/components/Graphics.js
@@ -8,27 +8,32 @@ const height = 500 - margin.top - margin.bottom;
 
 
 class Graphics extends Component {
-
+    
     constructor(props) {
         super(props);
-
+        
         this.state = {
             dummyData: []
         }
+        
+        
         this.x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
         this.y = d3.scaleLinear().range([height, 0]);
-        this.xAxis = d3.axisBottom(this.x);
+        this.xAxis = d3.axisBottom(this.x).ticks(10);
         this.yAxis = d3.axisLeft(this.y).ticks(10);
-        this.renderChart = this.renderChart.bind(this);
-           
+        this.renderAxis = this.renderAxis.bind(this);
+        this.renderBars = this.renderBars.bind(this);
+        
     }
     
     componentDidMount() {
+        
         this.svg = d3.select(this.refs.container)
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr( "transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr( "transform", "translate(" + margin.left + "," + margin.top + ")");
+        
         this.setState({
             dummyData: [
                 {
@@ -52,115 +57,83 @@ class Graphics extends Component {
                     modern: 80,
                     services: 10,
                     recurrence: 10
+                },
+                {
+                    name: 'third',
+                    price: 20,
+                    cozy: 10,
+                    luxury: 70,
+                    taste: 10,
+                    loud: 30,
+                    modern: 80,
+                    services: 10,
+                    recurrence: 10
                 }
             ]
         });
-        this.renderChart();
     }
     
     componentDidUpdate(){
-        this.renderChart();
         
+        this.renderAxis();
+        this.renderBars();
     }
     
-    renderChart() {
+    renderBars() {
         
-        // this.svg.selectAll('g')
-        // .data(this.state.dummyData)
-        // .enter()
-        // .append('circle').attr('transform', d => `translate(${d.price}, 20)`)
-        // .attr('cx',32).attr('cy',53).attr('r',15);
-        this.state.dummyData.forEach( (data) => {
+        this.svg.selectAll('bar')
+            .data(this.state.dummyData)
+            .enter()
+        .append('rect')
+            .style('fill', 'steelblue')
+            .attr('x', d => this.x(d.name))
+            .attr('width', this.x.bandwidth())
+            .attr('y', d => this.y(d.price))
+            .attr('height', d => ( height - this.y(d.price)) )
 
-            this.x.domain([0, 2]);
-            this.y.domain([0, 100]);
 
-            this.svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
+    }
+    
+    renderAxis() {
+        
+        this.x.domain(this.state.dummyData.map( d => d.name ));
+        this.y.domain([0, 100]);
+        
+        this.svg
+        .append('g')
+            .attr('class', 'x axis')
+            .attr('transform', `translate( 0 , ${height})`)
             .call(this.xAxis)
-            .selectAll("text")
+        .selectAll('text')
             .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", "-.55em")
-            .attr("transform", "rotate(-90)" );
+            .attr("dx", ".8em")
+            .attr("dy", "2em");
+        
+        const yColor = ['#FFC36F', '#FFC84D', '#FFC240', '#FFAB34', '#FF9127',
+        '#FF761B', '#FF5B13', '#FF4C0B', '#FF4A0B', '#FF3A06', '#FF0303']
 
-            this.svg.append("g")
-            .attr("class", "y axis")
+        this.svg
+        .append('g')
+            .attr('class', 'y axis')
             .call(this.yAxis)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Value ($)");
+        .selectAll('text')
+            .data(yColor)
+            .style('fill', c => c)
+            .style('font-size', '15px')
+            .style('font-weight', 'bold')
 
-            this.svg.selectAll("bar")
-            .data(data)
-            .enter().append("rect")
-            .style("fill", "steelblue")
-            .attr("x", function(d) { return this.x(d.name); })
-            .attr("width", this.x.bandwidth())
-            .attr("y", function(d) { return this.y(d.price); })
-            .attr("height", function(d) { return height - this.y(d.price); });
-        })
-        // this.x.domain( this.state.dummyData.map(d => d.name ));
-        // this.y.domain([0, 100]);
-
-        // this.svg.selectAll('g')
-        //     .data(this.state.dummyData)
-        //     .enter()
-        // .append('g')
-        //     .attr('class', 'x axis')
-        //     .call(this.xAxis)
-        // .selectAll("text")
-        //     .style("text-anchor", "end")
-        //     .attr("dx", "-.8em")
-        //     .attr("dy", "2em");
-
-        // this.svg.selectAll('g')
-        //     .attr('class', 'y axis')
+        
+        //     this.svg.append("g")
+        //     .attr("class", "y axis")
         //     .call(this.yAxis)
-        // .append('text')
-        //     .attr('transform', 'rotate(-90)')
-        //     .attr('y', 6)
-        //     .attr('dy', '.71em')
-        //     .style('text-anchor', 'end')
-        //     .text('Value (%)');
-
-    
-        //     this.x.domain(data.map(d => d.name));
-        //     this.y.domain([0, d3.max(data, d => d.price)]);
-    
-        //     this.svg.append("g")
-        //         .attr("class", "x axis")
-        //         .style("font:10px sans-serif")
-        //         .call(this.xAxis)
-        //     .selectAll("text")
-        //         .style("text-anchor", "end")
-        //         .attr("dx", "-.8em")
-        //         .attr("dy", "2em");
-    
-        //     this.svg.append("g")
-        //         .attr("class", "y axis")
-        //         .style("font:10px sans-serif")
-        //         .call(this.yAxis)
         //     .append("text")
-        //         .attr("transform", "rotate(-90)")
-        //         .attr("y", 6)
-        //         .attr("dy", ".71em")
-        //         .style("text-anchor", "end")
-        //         .text("Value (%)");
-    
-        //     this.svg.selectAll("bar")
-        //         .data(data)
-        //     .enter().append("rect")
-        //         .style("fill", "steelblue")
-        //         .attr("x", function(d) { return this.x(d.name); })
-        //         .attr("width", this.x.bandWidth())
-        //         .attr("y", function(d) { return this.y(d.price); })
-        //         .attr("height", function(d) { return height - this.y(d.price); });
-        // })
+        //     .attr("transform", "rotate(-90)")
+        //     .attr("y", 6)
+        //     .attr("dy", ".71em")
+        //     .style("text-anchor", "end")
+        //     .text("Value ($)");
+        
+        
     }
 
     render() {
