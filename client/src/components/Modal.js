26cdd2554e78d71732df481e6c44643e7eafa658
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-07-20 16:11:16 -0700

    add css on recur buttons

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -9,12 +9,19 @@ class Modal extends Component {
         this.state = {
             page: 1,
             price_score: 0,
+            price_comment: '',
             cozy_score: 0,
+            cozy_comment: '',
             luxury_score: 0,
+            luxury_comment: '',
             taste_score: 0,
+            taste_comment: '',
             loud_score: 0,
+            loud_comment: '',
             modern_score: 0,
+            modern_comment: '',
             services_score: 0,
+            services_comment: '',
             recurrence_score: false,
         }
 
@@ -171,14 +178,30 @@ class Modal extends Component {
                 <div className="modal-body">
                     <h1>Would you go there again? </h1>
                     <div style={{margin: '60px 0'}}>
-                        <button style={{fontSize: '80px', borderColor:'white'}} className="btn btn-success" onClick={ () => this.setState({ ...this.state, recurrence_score: true }) }>Yes</button>
-                        <button style={{fontSize: '80px', borderColor:'white'}} className="btn btn-danger" onClick={ () => this.setState({ ...this.state, recurrence_score: false }) }>No&nbsp;</button>
+                        <button style={{fontSize: '80px', borderColor:'white'}} className="btn btn-success" onClick={this.onRecurrenceButton}>Yes</button>
+                        <button style={{fontSize: '80px', borderColor:'white'}} className="btn btn-danger" onClick={this.onRecurrenceButton}>No&nbsp;</button>
                     </div>
                     <button className="btn btn-info" onClick={this.getPrevGauge}>Prev</button>
                     <button className="btn btn-lg btn-success" onClick={this.evaluate}>Submit</button>
                 </div>
             )
             break;
+        }
+    }
+
+    onRecurrenceButton = (e) => {
+        e.preventDefault();
+        e.currentTarget.style.borderWidth = '3px';
+        e.currentTarget.style.boxShadow = '0 0 10px rgb(255, 255, 255)';
+
+        if(e.currentTarget.innerHTML === 'Yes') {
+            e.currentTarget.nextSibling.style.borderWidth = '1px';
+            e.currentTarget.nextSibling.style.boxShadow = 'none';
+            this.setState({ ...this.state, recurrence_score: true });
+        }else {
+            e.currentTarget.previousSibling.style.borderWidth = '1px';
+            e.currentTarget.previousSibling.style.boxShadow = 'none';
+            this.setState({ ...this.state, recurrence_score: false });
         }
     }
 
