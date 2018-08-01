068b18f2a59d057d86d59be2677c7e4c42c8c75c
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-08-01 16:03:45 -0700

    download icons for food types

--- client/src/components/HomeModal.js
+++ client/src/components/HomeModal.js
@@ -6,7 +6,9 @@ class HomeModal extends Component {
     constructor(props){
         super(props);
         this.state = {
-            food_type: 'hamburger'
+            types: [ 'hamburgers','french','chinese','korean','franchise','japanese','vietnamese','spanish','brazilian','mexican','fine-dining','seafood','barbeque','fast-food','pizza','greek','ramen','buffet','food-court','steak-house','all-you-can-eat','food-truck','mongolian', 'breakfast', 'italian','sushi'],
+            selected_food_type: 'hamburgers',
+            img_url: 'https://us.123rf.com/450wm/timolina/timolina1504/timolina150400378/39128783-big-juicy-hamburger-with-vegetables-and-beef-on-a-wooden-background-in-rustic-style.jpg?ver=6'
         }
 
         this.renderFoodImage = this.renderFoodImage.bind(this);
@@ -16,6 +18,8 @@ class HomeModal extends Component {
         this.renderFoodImage();
     }
     renderFoodImage() {
+
+        const { img_url } = this.state
         
         d3.select('.left-container')
         .append('defs')
@@ -27,13 +31,14 @@ class HomeModal extends Component {
             .attr('x','0')
             .attr('y','0')
         .append('image')
-            .attr('xlink:href','https://us.123rf.com/450wm/timolina/timolina1504/timolina150400378/39128783-big-juicy-hamburger-with-vegetables-and-beef-on-a-wooden-background-in-rustic-style.jpg?ver=6')
+            .attr('xlink:href', img_url)
             .attr('width','300')
             .attr('height','400')
             .attr('preserveAspectRatio','xMinYMin slice')
     }
 
     render() {
+        const { types } = this.state;
         return (
             <main className="HomeModal">
                 <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#home_modal">
@@ -48,7 +53,37 @@ class HomeModal extends Component {
                                         <path d="M0,0h300l-30,400h-270v-400"></path>
                                     </g>
                                 </svg>
-                                <div className="right-container"></div>
+                                <div className="right-container">
+
+                                {
+                                    types.map( (d, i) => {
+                                        if( i % 2 === 0 ){
+                                            return d ? (
+                                                <div key={i} className="rows">
+                                                    <div style={{width: '30%', textAlign: 'center', margin: '0 15px'}}>
+                                                        <div id={d} style={{width: '50px', height: '50px', marginLeft: '20%', backgroundImage: `url(/assets/images/food_types/${d}.png)`, backgroundSize: 'cover'}}></div>
+                                                        <h6 style={{color: 'gray'}}>{d}</h6>
+                                                    </div>
+                                                    {
+                                                        types[i+1] ? (
+                                                            <div style={{width: '30%', textAlign: 'center', margin: '0 15px'}}>
+                                                                <div id={ types[i+1] } style={{width: '50px', height: '50px', marginLeft: '20%', backgroundImage: `url(/assets/images/food_types/${types[i+1]}.png)`, backgroundSize: 'cover'}}></div>
+                                                                <h6 style={{color: 'gray'}}>{types[i+1]}</h6>
+                                                            </div>
+                                                        )
+                                                        :
+                                                        ''
+                                                    }
+                                                </div>
+                                            )
+                                            :
+                                            ''
+                                        }
+                                    })
+                                }
+                                    <div style={{color: 'silver', fontSize: '8px'}}>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Meat">Meat</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                                    <div style={{color: 'silver', fontSize: '8px'}}>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Socrates">Socrates</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                                </div>
                             </div>
                         </div>
                     </div>
