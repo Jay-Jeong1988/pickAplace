3f1ab1d89a434bd54d3fc6638c7647ac44494af6
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-08-03 14:45:41 -0700

    complete second page for modal

--- client/src/components/HomeModalContentB.js
+++ client/src/components/HomeModalContentB.js
@@ -42,7 +42,7 @@ function HomeModalContentB(props) {
                     })
                 }
                     <div style={{color: 'silver', fontSize: '8px'}}>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
-                    <div style={{color: 'silver', fontSize: '8px'}}>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Socrates">Socrates</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
+                    <div style={{color: 'silver', fontSize: '8px'}}>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
                 </div>
             </div>
         </div>
